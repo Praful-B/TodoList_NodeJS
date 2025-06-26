29dc706717367e961b3df035c9898ec0f2ba2514
Praful-B <baleraoprafulchandra2006@gmail.com>
2025-06-27 04:42:57 +0530

    basic post handling

--- routes/login.js
+++ routes/login.js
@@ -1,10 +1,16 @@
 const express = require('express');
 const router = express.Router();
-const auth = require('/middleware/auth');
+const auth = require('../middleware/auth');
 
 
 router.get('/', auth,(req, res) => {
-    res.render('/login');
+    res.render('login');
+})
+
+
+router.post('/', auth,(req, res) => {
+    const { username, password } = req.body;
+    res.redirect('/home');
 })
 
 module.exports = router;
